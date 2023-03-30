dadb583ca871e40ba5803a894b104a9fa17d3f98
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-03-30 21:19:40 +0900

    feat: Added a method in Summon.cs to stop within the range after searching for the opponent and moving

--- Assets/Prefabs/Summons/Script/Summon.cs
+++ Assets/Prefabs/Summons/Script/Summon.cs
@@ -1,38 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 //소환수들의 공통적인 속성과 메서드를 다루는 스크립트
 public class Summon : MonoBehaviour
 {
     //ToDo: GameManager를 통해 픽 된 캐릿터 스탯 가져오기
-    private float[] stats = { 10f, 0.5f };  //임시 스탯 이동속도와 사거리
+    private float[] stats = { 0.3f};  //임시 스탯 이동속도
     //ToDo: GameManager에서 팀 판별 초기화
     private bool myteam;
-    private void Update()
+
+    private GameObject opponent;
+
+    private CircleCollider2D circleCollider;
+
+    private void Start()
     {
-        GameObject opponent = SearchOpponent();
-        MoveForOpponent(opponent);
+        circleCollider = GetComponentInChildren<CircleCollider2D>();
     }
+
+    private void FixedUpdate()
+    {
+        opponent = SearchOpponent();
+        MoveForOpponent();
+    }
+
     GameObject SearchOpponent()
     {
         GameObject[] opponents = GameObject.FindGameObjectsWithTag("Summon");
         float nearestdistance = Mathf.Infinity;
         GameObject nearestOpponent = opponents[0];
-        
+
         //ToDo: 팀 판별
-        foreach (GameObject opponent in opponents) {
+        foreach (GameObject opponent in opponents)
+        {
+            if (opponent == gameObject) continue;
+
             float distance = Vector2.Distance(transform.position, opponent.transform.position);
-            if (nearestdistance>distance)
+            if (nearestdistance > distance)
             {
                 nearestdistance = distance;
                 nearestOpponent = opponent;
             }
         }
+
         return nearestOpponent;
     }
-    void MoveForOpponent(GameObject opponent)
+
+    void MoveForOpponent()
     {
-        transform.position = Vector2.MoveTowards(transform.position, opponent.transform.position, stats[0] * Time.deltaTime);
+        if (opponent != null)
+        {
+            float distance = Vector2.Distance(transform.position, opponent.transform.position);
+
+            if (distance <= circleCollider.radius)
+            {
+                // 사거리 이내에 있으면 이동을 멈춤
+                return;
+            }
+
+            Vector3 direction = (opponent.transform.position - transform.position).normalized;
+            transform.position += direction * stats[0] * Time.deltaTime;
+        }
     }
 }
