57a759cf4d1723b6c0f3223b8c21298c677172c3
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-03-30 18:24:23 +0900

    feat: Scriptable objects per summon & GameManger.cs

---
+++ Assets/BattleScene/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Singleton Instance 선언
+    public static GameManager instance = null;
+    [Header("# Game Control")]
+    public float GameTime;
+    public float MaxGameTime = 60 * 3f;   //전투시간
+
+    private void Awake()
+    {
+        instance = this;
+        // Scene 이동 시 삭제 되지 않도록 처리
+        //DontDestroyOnLoad(this.gameObject);
+    }
+    private void Update()
+    {
+        GameTime += Time.deltaTime;
+        if (GameTime > MaxGameTime)
+        {
+            GameTime = MaxGameTime;
+        }
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Resources/SummonsData/SummonScriptableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Summon", menuName = "ScriptableObjects/Summon")]
+public class SummonScriptableObject : ScriptableObject
+{
+    [Header("Summon Stats")]
+    public float AttackRange;
+    public float SummonSpeed;
+}
