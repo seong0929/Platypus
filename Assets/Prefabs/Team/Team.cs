4f0e3016b35ce50511c59cc46f5063550af122ed
KimDa99 <hyhy61451@gmail.com>
2023-05-24 00:06:06 +0900

    Revert "feat: work on player's on the team"
    This reverts commit 818da9e90286cb16f023ac6e8cafd9d6c261b61d.

--- Assets/Prefabs/Team/Team.cs
+++ Assets/Prefabs/Team/Team.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Team : MonoBehaviour
 {
-    //PlayerLimit
-    const int MAX_PLAYER = 10;
-    const int MAX_ROSTER = 6;
-
     public string Name { get; set; } // The name of the team
     public Enums.ELeague League { get; set; } // The league the team belongs to -- might need furthur disccusion --- needs ENUM
     public int Reputation { get; set; } // The reputation of the team 
@@ -21,6 +17,7 @@ public class Team : MonoBehaviour
     public Enums.ESponsor Sponsor { get; set; } // The sponsor of the team
 
 
+
     // Coach 
     public Coach Coach { get; set; } // The coach of the team
 
@@ -32,71 +29,4 @@ public class Team : MonoBehaviour
 
     // Equipment
     public List<Enums.EEquipment> Equipment { get; set; } // The equipment owned by the team
-
-
-    public void ScoutPlayer(Player player)
-    {
-        if(MAX_PLAYER <= Players.Count)
-        {
-            Debug.LogWarning("Team is already Fulled! the player can not be scouted");
-            return;
-        }
-        if(player = null)
-        {
-            Debug.LogWarning("The player is invalid, can not be scouted");
-            return;
-        }
-
-        Players.Add(player);
-        return;
-    }
-    public void ReleasePlayer(Player player)
-    {
-        if (Players.Contains(player)!)
-        {
-            Debug.LogWarning("No Such player in this team. The player can not be released");
-            return;
-        }
-
-        Players.Remove(player);
-        return;
-    }
-
-    public void AddPlayerOnRoaster(Player player)
-    {
-        if(Players.Contains(player)!)
-        {
-            Debug.LogWarning("No Such player in this team. The player can not be one the roaster");
-            return;
-        }
-        if( MAX_ROSTER <= Players.Count)
-        {
-            Debug.LogWarning("Roaster is already Fulled!");
-            return;
-        }
-
-        Players.Add(player);
-        return;
-    }
-    public void RemovePlayerOnRoster(Player player)
-    {
-        if (Roster.Contains(player)!)
-        {
-            Debug.LogWarning("No Such player in the Roaster");
-            return;
-        }
-
-        Roster.Remove(player);
-        return;
-    }
-
-    public bool IsRosterValid(int minNum)
-    {
-        if(Roster.Count < minNum)
-        {
-            return false;
-        }
-        return true;
-    }
-
 }
