120bb821bb49fa7a141938b5b0168978940ac907
KimDa99 <hyhy61451@gmail.com>
2023-05-24 17:40:33 +0900

    add: MatchManager

---
+++ Assets/Game/MatchManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    private Group groupA;
+    private Group groupB;
+
+//    private int PlayerNum;
+
+    MatchManager(Group a, Group b)//, int playerNum)
+    {
+        groupA = a;
+        groupB = b;
+//        PlayerNum = playerNum;
+    }
+    public void SetSelectedPlayer(bool isA, List<Player> selectedOne)
+    {
+
+    }
+    public void SetBanPick()
+    {
+
+    }
+    /*
+    Team (coach, player, roster), 
+    n:n(playerNum), 
+    Selected Player, 
+    Ban&Pick, 
+    wiring player&Summon
+     */ 
+
+}
+
+public class Group
+{
+    public Team Team;
+    public List<Player> SelectedPlayers;
+    public List<Enums.ESummon> SelectedSummon;
+    public List<Enums.ESummon> BannedSummon;
+
+    Group(Team team, List<Player> selecetedOne)
+    {
+        Team = team;
+        SelectedPlayers = selectedOne;
+        SelectedSummon = new List<Enums.ESummon>();
+        BannedSummon = new List<Enums.ESummon>();
+    }
+
+}
