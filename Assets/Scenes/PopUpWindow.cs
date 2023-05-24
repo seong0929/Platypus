569a594d1bfb8ba986ba7ab7efb22223b0650842
KimDa99 <hyhy61451@gmail.com>
2023-05-24 16:28:13 +0900

    refactor: changed file structure for scene & Home Scouting Window adjustment

--- Assets/Scenes/PopUpWindow.cs
+++ Assets/Scenes/PopUpWindow.cs
@@ -1,24 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class PopUpWindow: MonoBehaviour
 {
     [SerializeField]
     private GameObject Window;
+    [SerializeField]
+    private GameObject Content;
 
-    // Start is called before the first frame update
-    /*
-    void Start()
-    {
-
-    }
-     * 
-     */
 
     public void ToggleWindow()
     {
         Window.SetActive(!Window.activeSelf);
+        Content.SetActive(!Window.activeSelf);
     }
-
 }
