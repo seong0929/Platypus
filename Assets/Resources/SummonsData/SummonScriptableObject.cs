6d5241502af5f3aa7c7b4cf8574f90069311692a
KimDa99 <hyhy61451@gmail.com>
2023-11-05 21:24:30 +0900

    feat: add Crystal Price in summmons

--- Assets/Resources/SummonsData/SummonScriptableObject.cs
+++ Assets/Resources/SummonsData/SummonScriptableObject.cs
@@ -16,5 +16,6 @@ public class SummonScriptableObject : ScriptableObject
     [Header("Types")]
     public Enums.EElement ElementType;
 
-    // sprite
+    [Header("Price")]
+    public int CrystalNum;
 }
